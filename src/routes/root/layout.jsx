82cc02cf33b6b4ff15df6f58139a5d35c6f29e60
Harsh Gaur <hgaur491@gmail.com>
2025-01-28 22:37:28 +0530

    adding sonner toaster

--- src/routes/root/layout.jsx
+++ src/routes/root/layout.jsx
@@ -1,5 +1,7 @@
 import Sidebar from "@/components/partials/Sidebar";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Toaster } from "@/components/ui/sonner"
+
 export default function RootLayout({ children }) {
     return (
         <>
@@ -8,6 +10,7 @@ export default function RootLayout({ children }) {
             <div className="fixed bottom-4 right-4">
                 <ModeToggle />
             </div>
+            <Toaster />
         </>
     );
 }
