2f25226b950b2b33b5e00e6e29b4f3c79e835247
Harsh Gaur <hgaur491@gmail.com>
2025-02-01 21:45:21 +0530

    some fixes in sidebar

--- src/components/partials/Sidebar.jsx
+++ src/components/partials/Sidebar.jsx
@@ -22,34 +22,33 @@ export default function Sidebar({ children }) {
     }, [setIsSidebarOpen]);
     return (
         <>
-            <div
-                className={`
-                    pt-14 flex flex-col z-50 fixed inset-0 h-screen w-72 bg-background transition-transform duration-300 ease-in-out
+            <TopNavbar className={`z-50 fixed top-0 left-0 right-0 h-14 bg-background`} />
+            <>
+                <div
+                    className={`
+                    pt-14 flex flex-col z-40 fixed inset-0 h-screen w-72 bg-background transition-all duration-300 ease-in-out
                     ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}
                 `}
-            >
-                <ScrollArea className="px-4 py-0">
-                    {Array.from({ length: 10 }, (_, i) => (
-                        <div key={i} className="h-20">item</div>
-                    ))}
-                    <div>last</div>
-                </ScrollArea>
-            </div>
+                >
+                    <ScrollArea className="px-4 py-0">
+                        {Array.from({ length: 10 }, (_, i) => (
+                            <div key={i} className="h-20">
+                                item
+                            </div>
+                        ))}
+                        <div>last</div>
+                    </ScrollArea>
+                </div>
+                <div
+                    className={`${
+                        isSidebarOpen ? "z-40 fixed inset-0 bg-background opacity-[0.4]" : "opacity-0"
+                    } transition-all duration-300 lg:hidden`}
+                    onClick={() => setIsSidebarOpen(false)}
+                ></div>
+            </>
             <div
                 className={`${
-                    isSidebarOpen ? "z-40 fixed inset-0 bg-background opacity-[0.4]" : "opacity-0"
-                } transition-opacity duration-300 md:hidden`}
-                onClick={() => setIsSidebarOpen(false)}
-            ></div>
-            <TopNavbar
-                className={`${
-                    isSidebarOpen ? "" : "md:ml-0"
-                } transition-all duration-300 ease-in-out z-50 fixed top-0 left-0 right-0 h-14 bg-background `}
-            />
-
-            <div
-                className={`${
-                    isSidebarOpen ? "md:ml-72 md:h-auto" : "md:ml-0"
+                    isSidebarOpen ? "lg:ml-72 lg:h-auto" : "lg:ml-0"
                 } transition-all duration-300 ease-in-out`}
             >
                 {children}
