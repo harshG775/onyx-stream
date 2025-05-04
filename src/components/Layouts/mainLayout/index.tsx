2bb8fb763d6d61afc4a64b6ff3fdf21428e166c7
Harsh Gaur <hgaur491@gmail.com>
2025-05-04 21:00:05 +0530

    feat: refactor Navbar and Sidebar components, integrating new Sidebar structure and context for improved state management

--- src/components/Layouts/mainLayout/index.tsx
+++ src/components/Layouts/mainLayout/index.tsx
@@ -1,13 +1,86 @@
-import MainNavbar from "./Navbar";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
-type MainLayoutProps = {
-    children: React.ReactNode;
-};
+import Link from "next/link";
+import { ListVideoIcon, MoveIcon, TvIcon } from "lucide-react";
 
-export default function MainLayout({ children }: MainLayoutProps) {
+export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
-        <>
-            <MainNavbar>{children}</MainNavbar>
-        </>
+        <SidebarProvider>
+            <AppSidebar />
+            <main className="pt-16">
+                <header className="bg-background text-foreground fixed left-0 right-0 top-0 z-50 h-16 flex items-center gap-2 p-2">
+                    <SidebarGroup className="flex-row items-center gap-1">
+                        <SidebarTrigger />
+                        <LogoIcon />
+                    </SidebarGroup>
+                </header>
+                {children}
+            </main>
+        </SidebarProvider>
     );
 }
+
+const StreamMode = [
+    { name: "TV", id: "tv", Icon: TvIcon },
+    { name: "Movie", id: "movie", Icon: MoveIcon },
+    { name: "Anime", id: "anime", Icon: TvIcon },
+    { name: "Live", id: "live", Icon: ListVideoIcon },
+];
+function AppSidebar() {
+    return (
+        <Sidebar className="border-none">
+            <SidebarHeader>
+                <SidebarGroup className="flex-row items-center gap-1">
+                    <SidebarTrigger />
+                    <LogoIcon />
+                </SidebarGroup>
+            </SidebarHeader>
+            <SidebarContent className=" bg-background text-foreground">
+                <SidebarGroup>
+                    <SidebarMenu>
+                        {StreamMode.map(({ id, name, Icon }) => (
+                            <SidebarMenuItem key={id}>
+                                <SidebarMenuButton asChild>
+                                    <Link href={`/${id}`}>
+                                        <Icon className="size-4" />
+                                        <span>{name}</span>
+                                    </Link>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter />
+        </Sidebar>
+    );
+}
+
+const LogoIcon = () => (
+    <Link href="/" className="group flex h-8 items-center rounded-md px-4 py-2 text-xl font-bold cursor-pointer">
+        <div className="text-primary">Onyx</div>
+        Stream
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="640"
+            height="512"
+            viewBox="0 0 640 512"
+            className="size-6 transition-colors duration-400 group-hover:text-primary group-focus-visible:text-primary"
+        >
+            <path
+                fill="currentColor"
+                d="m352 124.5l-51.9-13c-6.5-1.6-11.3-7.1-12-13.8s2.8-13.1 8.7-16.1l40.8-20.4l-43.2-32.4c-5.5-4.1-7.8-11.3-5.6-17.9S297.1 0 304 0h160c30.2 0 58.7 14.2 76.8 38.4l57.6 76.8c6.2 8.3 9.6 18.4 9.6 28.8c0 26.5-21.5 48-48 48h-21.5c-17 0-33.3-6.7-45.3-18.7L480 160h-32v21.5c0 24.8 12.8 47.9 33.8 61.1l106.6 66.6c32.1 20.1 51.6 55.2 51.6 93.1c0 60.6-49.1 109.7-109.8 109.7H32.3c-3.3 0-6.6-.4-9.6-1.4c-9.2-2.8-16.7-9.6-20.3-18.5C1 488.7.2 485.2 0 481.4c-.2-3.7.3-7.3 1.3-10.7c2.8-9.2 9.6-16.7 18.6-20.4c3-1.2 6.2-2 9.5-2.2L433.3 412c8.3-.7 14.7-7.7 14.7-16.1c0-4.3-1.7-8.4-4.7-11.4l-44.4-44.4c-30-30-46.9-70.7-46.9-113.1z"
+            />
+        </svg>
+    </Link>
+);
