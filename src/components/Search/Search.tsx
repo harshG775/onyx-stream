7bf8c0a74587d7495b9cf4b7c75c2ed4824df8e3
Harsh Gaur <hgaur491@gmail.com>
2025-05-05 21:04:42 +0530

    feat: enhance Search component with loading and error states, and add media type filters

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
-import { SearchIcon } from "lucide-react";
+import { Loader, SearchIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { use, useEffect, useState } from "react";
+import { useState } from "react";
 import { AppLogo } from "../ui/AppLogo";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
@@ -18,7 +18,7 @@ export default function Search({ className }: { className?: string }) {
     const [query, setQuery] = useState("");
     const [debouncedInput] = useDebounce(query, 500);
 
-    const { data } = useQuery({
+    const { isLoading, isError, data } = useQuery({
         queryKey: ["search", debouncedInput, mediaType],
         queryFn: async ({ signal }) => {
             const data = await axios.get(`https://api.themoviedb.org/3/search/${mediaType}`, {
@@ -67,7 +67,24 @@ export default function Search({ className }: { className?: string }) {
                             <Button>Search</Button>
                         </form>
                     </SheetHeader>
-                    <SearchResult media={data?.results || []} />
+                    <div className="flex gap-2 px-4 mb-2 ">
+                        <Button
+                            variant={mediaType === "multi" ? "default" : "outline"}
+                            onClick={() => setMediaType("multi")}
+                        >
+                            All
+                        </Button>
+                        <Button variant={mediaType === "tv" ? "default" : "outline"} onClick={() => setMediaType("tv")}>
+                            Tv Series
+                        </Button>
+                        <Button
+                            variant={mediaType === "movie" ? "default" : "outline"}
+                            onClick={() => setMediaType("movie")}
+                        >
+                            Movie
+                        </Button>
+                    </div>
+                    <SearchResult isLoading={isLoading} isError={isError} media={data?.results || []} />
                 </SheetContent>
             </Sheet>
         </div>
@@ -85,6 +102,8 @@ type MediaDataType = {
     first_air_date: string;
 };
 type SearchResultProps = {
+    isLoading: boolean;
+    isError: boolean;
     media: MediaDataType[];
 };
 const MEDIA_TYPE = {
@@ -93,45 +112,54 @@ const MEDIA_TYPE = {
     person: "person",
     multi: "multi",
 };
-function SearchResult({ media }: SearchResultProps) {
+function SearchResult({ isLoading, isError, media }: SearchResultProps) {
     return (
         <div className="p-4 overflow-auto max-h-96 bg-accent">
-            {media?.map((media) => {
-                if (media.media_type === "person") return null;
-                return (
-                    <Link
-                        tabIndex={0}
-                        key={media.id}
-                        className=" odd:bg-background even:bg-accent hover:bg-primary/20 text-secondary-foreground flex gap-2 cursor-pointer p-2 rounded"
-                        href={`/${MEDIA_TYPE?.[media.media_type]}/${media.id}`}
-                        title={media.overview}
-                    >
-                        {media.poster_path && (
-                            // eslint-disable-next-line @next/next/no-img-element
-                            <img
-                                src={"https://image.tmdb.org/t/p/w200" + media.poster_path}
-                                alt={media.title}
-                                className="w-20 h-20 object-cover rounded"
-                            />
-                        )}
-                        <div>
-                            <div className="font-bold">{media.name || media.title}</div>
-                            <div className="text-sm font-sans">{media.release_date || media.first_air_date}</div>
-                            <div className="text-sm uppercase font-semibold">{media.media_type}</div>
-                        </div>
-                    </Link>
-                );
-            })}
+            {isLoading ? (
+                <div className="flex justify-center gap-2">
+                    <Loader className="animate-spin" />
+                    <span className="sr-only">Loading</span>
+                </div>
+            ) : (
+                <>
+                    {isError && <div className="flex justify-center gap-2 text-destructive">Something went wrong</div>}
+                    {media?.length === 0 && (
+                        <div className="flex justify-center gap-2 text-muted-foreground">No results</div>
+                    )}
+                    {media?.map((media) => {
+                        if (media.media_type === "person") return null;
+                        return (
+                            <Link
+                                tabIndex={0}
+                                key={media.id}
+                                className=" odd:bg-background even:bg-accent hover:bg-primary/20 text-secondary-foreground flex gap-2 cursor-pointer p-2 rounded"
+                                href={`/${MEDIA_TYPE?.[media.media_type]}/${media.id}`}
+                                title={media.overview}
+                            >
+                                {media.poster_path ? (
+                                    // eslint-disable-next-line @next/next/no-img-element
+                                    <img
+                                        src={"https://image.tmdb.org/t/p/w200" + media.poster_path}
+                                        alt={media.title}
+                                        className="w-20 h-20 object-cover rounded"
+                                    />
+                                ) : (
+                                    <div className="w-20 h-20 bg-muted-foreground/20 text-sm rounded flex justify-center items-center">
+                                        No Image
+                                    </div>
+                                )}
+                                <div>
+                                    <div className="font-bold">{media.name || media.title}</div>
+                                    <div className="text-sm font-sans">
+                                        {media.release_date || media.first_air_date}
+                                    </div>
+                                    <div className="text-sm uppercase font-semibold">{media.media_type}</div>
+                                </div>
+                            </Link>
+                        );
+                    })}
+                </>
+            )}
         </div>
     );
 }
-
-// import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-// const Tooltip =()=>{
-//     return (
-//         <Popover>
-//             <PopoverTrigger>Open</PopoverTrigger>
-//             <PopoverContent>Place content for the popover here.</PopoverContent>
-//         </Popover>
-//     );
-// }
