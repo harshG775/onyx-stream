1677d8a1c16f481c0771dcbefd1557319eb67dbe
Harsh Gaur <hgaur491@gmail.com>
2025-05-10 14:51:21 +0530

    feat: implement ThumbCarousel component and integrate it into HeroSection

--- src/components/home/HeroSection/index.tsx
+++ src/components/home/HeroSection/index.tsx
@@ -1,9 +1,6 @@
-"use client";
-
-import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
-import { useCallback, useEffect, useState } from "react";
+import { CarouselContent, CarouselItem, CarouselThumbContent, CarouselThumbItem, ThumbCarousel } from "./ThumbCarousel";
 
 const images = ["/img1.jpg", "/img2.jpg", "/img3.jpg", "/img4.jpg", "/img5.jpg", "/img6.jpg", "/img7.jpg"];
 
@@ -11,117 +8,22 @@ type HeroSectionPropsType = {
     className?: string;
 };
 export default function HeroSection({ className }: HeroSectionPropsType) {
-    return <CarouselThumb className={cn("w-full max-w-5xl mx-auto space-y-4", className)} />;
-}
-
-
-
-
-
-
-
-
-
-
-
-function CarouselThumb({ className }: { className?: string }) {
-    const [selectedIndex, setSelectedIndex] = useState(0);
-    const [mainRef, mainApi] = useEmblaCarousel();
-    const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
-        containScroll: "keepSnaps",
-        dragFree: true,
-    });
-
-    const onThumbClick = useCallback(
-        (index: number) => {
-            if (!mainApi || !emblaThumbsApi) return;
-            mainApi.scrollTo(index);
-        },
-        [mainApi, emblaThumbsApi]
-    );
-
-    const onSelect = useCallback(() => {
-        if (!mainApi || !emblaThumbsApi) return;
-        setSelectedIndex(mainApi.selectedScrollSnap());
-        emblaThumbsApi.scrollTo(mainApi.selectedScrollSnap());
-    }, [mainApi, emblaThumbsApi, setSelectedIndex]);
-
-    useEffect(() => {
-        if (!mainApi) return;
-        onSelect();
-
-        mainApi.on("select", onSelect).on("reInit", onSelect);
-    }, [mainApi, onSelect]);
-
     return (
-        <div className={cn("relative", className)}>
-            {/* Main Carousel */}
-            <div ref={mainRef} className="overflow-hidden rounded-lg">
-                <div className="flex touch-pan-y touch-pinch-zoom">
-                    {images.map((src, index) => (
-                        <Main key={index} index={index} src={src} />
-                    ))}
-                </div>
-            </div>
-
-            {/* Thumbnails Carousel */}
-            <div ref={emblaThumbsRef} className="overflow-hidden">
-                <div className="flex gap-2 px-2">
-                    {images.map((src, index) => (
-                        <Thumb
-                            key={index}
-                            onClick={() => onThumbClick(index)}
-                            selected={index === selectedIndex}
-                            index={index}
-                            src={src}
-                        />
-                    ))}
-                </div>
-            </div>
-        </div>
+        <ThumbCarousel className={cn("w-full max-w-7xl mx-auto space-y-4", className)}>
+            <CarouselContent>
+                {Array.from({ length: images.length }).map((_, index) => (
+                    <CarouselItem key={index}>
+                        <Image src={images[index]} alt={`Image ${index}`} width={500} height={400} />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselThumbContent>
+                {Array.from({ length: images.length }).map((_, index) => (
+                    <CarouselThumbItem key={index} index={index}>
+                        <Image src={images[index]} alt={`Image ${index}`} width={100} height={100} className="rounded border border-primary w-full"/>
+                    </CarouselThumbItem>
+                ))}
+            </CarouselThumbContent>
+        </ThumbCarousel>
     );
 }
-
-type MainPropsType = {
-    index: number;
-    src: string;
-};
-
-export const Main: React.FC<MainPropsType> = ({ index, src }) => {
-    return (
-        <div className="flex-[0_0_100%] h-96 min-w-0 border-2 border-primary" role="group">
-            <Image
-                src={src}
-                alt={`Image ${index}`}
-                width={800}
-                height={500}
-                className="w-full h-auto object-cover rounded-lg"
-            />
-        </div>
-    );
-};
-
-type ThumbPropType = {
-    selected: boolean;
-    index: number;
-    onClick: () => void;
-    src: string;
-};
-
-export const Thumb: React.FC<ThumbPropType> = (props) => {
-    const { selected, index, onClick, src } = props;
-    return (
-        <div
-            className={cn(
-                "flex-[0_0_40%] sm:flex-[0_0_30%] md:flex-[0_0_30%] lg:flex-[0_0_20%] xl:flex-[0_0_15%]",
-                "transition border-2 rounded-md overflow-hidden",
-                selected ? "border-primary" : "border-transparent opacity-70 hover:opacity-100"
-            )}
-            onClick={onClick}
-        >
-            <button key={index}>
-                <Image src={src} alt={`Thumbnail ${index}`} width={100} height={60} className="object-cover" />
-            </button>
-        </div>
-    );
-};
