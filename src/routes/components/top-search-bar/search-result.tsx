4b88c0086792c7693ef05856335c122fe754cf01
harshG775 <hgaur491@gmail.com>
2025-12-19 18:08:42 +0530

    //

--- src/routes/components/top-search-bar/search-result.tsx
+++ src/routes/components/top-search-bar/search-result.tsx
@@ -2,6 +2,7 @@ export function SearchResult({ query }: { query: string }) {
     console.log(query)
     return (
         <div className="bg-muted">
+            {query}
             <p className="text-sm text-muted-foreground p-4 text-center">
                 Try searching for a title, genre, or person.
             </p>
