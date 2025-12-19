a914fb7b02361d3642de8fb0d1886619e6056f9f
harshG775 <hgaur491@gmail.com>
2025-12-19 15:26:28 +0530

    fix:(page loader):loading

--- src/components/PagesTopLoader.tsx
+++ src/components/PagesTopLoader.tsx
@@ -1,11 +1,36 @@
 import { useRouterState } from "@tanstack/react-router"
-import { Progress } from "./ui/progress"
+import { Progress } from "@/components/ui/progress"
+import { useEffect, useRef, useState } from "react"
 
 export function PagesTopLoader() {
     const { isLoading } = useRouterState()
-    if (!isLoading) {
-        return null
-    }
+    const [value, setValue] = useState(0)
+    const timerRef = useRef<NodeJS.Timeout | null>(null)
 
-    return <Progress value={33} className="absolute top-0 left-0 right-0" />
+    useEffect(() => {
+        if (isLoading) {
+            setValue(30)
+
+            timerRef.current = setInterval(() => {
+                setValue((prev) => (prev < 90 ? prev + 5 : prev))
+            }, 200)
+        } else {
+            setValue(100)
+
+            if (timerRef.current) {
+                clearInterval(timerRef.current)
+                timerRef.current = null
+            }
+
+            const timeout = setTimeout(() => {
+                setValue(0)
+            }, 300)
+
+            return () => clearTimeout(timeout)
+        }
+    }, [isLoading])
+
+    if (!isLoading && value === 0) return null
+
+    return <Progress value={value} className="fixed top-0 left-0 right-0 z-50 h-1 rounded-none" />
 }
