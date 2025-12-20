0737ea96ef19769e02776a245aee5cc01f5a6b13
Harsh_G <hgaur491@gmail.com>
2025-12-20 12:50:07 +0530

    updating meta

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -15,26 +15,57 @@ interface MyRouterContext {
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-    head: () => ({
-        meta: [
-            {
-                charSet: "utf-8",
-            },
-            {
-                name: "viewport",
-                content: "width=device-width, initial-scale=1",
-            },
-            {
-                title: "OnyxStream",
-            },
-        ],
-        links: [
-            {
-                rel: "stylesheet",
-                href: appCss,
-            },
-        ],
-    }),
+    loader: async ({ location }) => {
+        return { host: location.url.origin }
+    },
+    head: async ({ loaderData }) => {
+        const title = "OnyxStream – Watch Movies & TV Shows Online"
+        const description =
+            "Stream the latest movies and TV shows on OnyxStream. Discover trending, popular, and top-rated content in one place."
+
+        const poster = "branding/logo/logo-icon.svg"
+
+        const url = `${loaderData?.host}`
+
+        return {
+            meta: [
+                {
+                    charSet: "utf-8",
+                },
+                {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1",
+                },
+                // SEO
+                { title: title },
+                { name: "description", content: description },
+                {
+                    name: "keywords",
+                    content: "movies, tv shows, streaming, watch online, latest movies, series, OnyxStream",
+                },
+
+                // Open Graph
+                { property: "og:type", content: "video.movie" },
+                { property: "og:title", content: title },
+                { property: "og:description", content: description },
+                { property: "og:image", content: poster },
+                { property: "og:url", content: url },
+
+                // Twitter
+                { name: "twitter:card", content: "summary_large_image" },
+                { name: "twitter:title", content: title },
+                { name: "twitter:description", content: description },
+                { name: "twitter:image", content: poster },
+                { name: "twitter:url", content: url },
+            ],
+            links: [
+                {
+                    rel: "stylesheet",
+                    href: appCss,
+                },
+            ],
+        }
+    },
 
     shellComponent: RootDocument,
     notFoundComponent: () => {
@@ -43,7 +74,6 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 })
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-
     return (
         <html lang="en">
             <head>
