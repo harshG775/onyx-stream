7f45fe147ac05756d5d1bf0ca47c5212f69622fe
Harsh_G <hgaur491@gmail.com>
2025-12-22 23:17:00 +0530

    update(settings): updating them toggle ui

--- src/routes/settings/index.tsx
+++ src/routes/settings/index.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from "@/components/contexts/theme-provider"
+import { cn } from "@/lib/utils"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/settings/")({
@@ -6,19 +7,45 @@ export const Route = createFileRoute("/settings/")({
 })
 
 function RouteComponent() {
-      const { theme, setTheme } = useTheme()
+    const { theme, setTheme } = useTheme()
 
     return (
-        <main>
-            <h1>Settings</h1>
-            <section>
-                <h2>Appearance:{theme}</h2>
-                <div>
-                    <div onClick={()=>setTheme("dark")}>Dark</div>
-                    <div onClick={()=>setTheme("light")}>Light</div>
-                    <div>System Default</div>
-                </div>
-            </section>
-        </main>
+        <div className="px-3 sm:px-4 lg:px-6 py-3 sm:py-4 ">
+            <main className="p-4 max-w-[96rem] mx-auto bg-card shadow border rounded-md">
+                <h1 className="font-semibold text-xl">Settings</h1>
+                <section className="mt-2">
+                    <div>
+                        <h2 className="font-medium text-lg text-secondary-foreground">Appearance</h2>
+                        <div className="h-0.5 bg-border rounded-full my-3" />
+                    </div>
+                    <div>
+                        <div>
+                            <div className="font-medium">Color Mode</div>
+                            <p className="font-medium text-muted-foreground text-sm">Select Your Default Theme</p>
+                        </div>
+                        <div className="mt-2 flex flex-wrap gap-4 max-w-sm">
+                            <div className="flex-1 h-full aspect-16/10 flex flex-col" onClick={() => setTheme("dark")}>
+                                <div
+                                    className={cn(
+                                        "shadow rounded sm:rounded-md bg-gray-900 flex-1",
+                                        theme === "dark" && "border-2 border-primary/60",
+                                    )}
+                                ></div>
+                                <div className="text-center text-sm mt-1">Dark</div>
+                            </div>
+                            <div className="flex-1 h-full aspect-16/10 flex flex-col" onClick={() => setTheme("light")}>
+                                <div
+                                    className={cn(
+                                        "shadow rounded sm:rounded-md bg-gray-100 flex-1",
+                                        theme === "light" && "border-2 border-primary/60",
+                                    )}
+                                ></div>
+                                <div className="text-center text-sm mt-1">Light</div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
     )
 }
