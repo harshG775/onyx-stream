e8f6d5f22e31dc31be9cecd05f0d835809a8a190
harshG775 <hgaur491@gmail.com>
2026-04-28 18:37:33 +0530

    Enhance PlayerFrame layout and styling; update TvControls border radius

--- src/routes/$media_type/$id/watch/-components/player-frame.tsx
+++ src/routes/$media_type/$id/watch/-components/player-frame.tsx
@@ -20,21 +20,27 @@ export function PlayerFrame({ sources }: Props) {
                 </div>
             )}
 
-            <iframe
-                key={active.url}
-                src={active.url}
-                allowFullScreen
-                allow="autoplay; fullscreen; picture-in-picture"
-                referrerPolicy="origin"
-                onLoad={() => setIsLoading(false)}
-                className={cn("w-full h-full aspect-video rounded-none xl:rounded-xl shadow-2xl", isLoading ? "hidden" : "")}
-            />
+            <div
+                className={cn(
+                    "w-full h-full aspect-video rounded-none xl:rounded-xl",
+                    isLoading ? "hidden" : "",
+                )}
+            >
+                <iframe
+                    key={active.url}
+                    src={active.url}
+                    allowFullScreen
+                    allow="autoplay; fullscreen; picture-in-picture"
+                    referrerPolicy="origin"
+                    onLoad={() => setIsLoading(false)}
+                    className={"w-full h-full aspect-video rounded-none xl:rounded-xl shadow-lg"}
+                />
+            </div>
 
             {sources.length > 1 && (
-                <div className="flex flex-col sm:flex-row sm:items-center gap-2 px-4">
-                    <span className="text-sm font-medium text-muted-foreground">Source</span>
-
-                    <div className="flex gap-2 overflow-x-auto scrollbar-hide">
+                <div className="px-4">
+                    <span className="text-sm font-bold text-muted-foreground">Source</span>
+                    <div className="flex flex-wrap items-center gap-2 ">
                         {sources.map((src, i) => (
                             <button
                                 key={src.id}
@@ -43,10 +49,10 @@ export function PlayerFrame({ sources }: Props) {
                                     setActiveIndex(i)
                                 }}
                                 className={cn(
-                                    "px-2 py-1",
+                                    "px-2 py-1 text-xs ",
                                     i === activeIndex
                                         ? "bg-primary text-primary-foreground border-primary"
-                                        : "bg-background border-border",
+                                        : "bg-muted text-muted-foreground border-border",
                                 )}
                             >
                                 {src.name}
