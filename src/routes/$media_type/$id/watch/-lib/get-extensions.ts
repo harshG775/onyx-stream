3badf85fdb75119797ee96c946fa360b05496d7e
Harsh_G <hgaur491@gmail.com>
2026-05-02 21:17:52 +0530

    add: new source

--- src/routes/$media_type/$id/watch/-lib/get-extensions.ts
+++ src/routes/$media_type/$id/watch/-lib/get-extensions.ts
@@ -4,14 +4,26 @@ const extension: ExtensionManifest = {
     version: "1.0",
     extensions: [
         {
+            id: "vidking",
+            name: "VidKing",
+            enabled: true,
+            required: false,
+            priority: 1,
+            baseUrl: "https://www.vidking.net/embed",
+            embedPatterns: {
+                movie: "{baseUrl}/movie/{id}",
+                tv: "{baseUrl}/tv/{id}/{season}/{episode}",
+            },
+        },
+        {
             id: "vidfast",
             name: "VidFast",
             enabled: true,
             required: false,
-            priority: 1,
+            priority: 2,
             baseUrl: "https://vidfast.pro",
             embedPatterns: {
-                movie: "{baseUrl}/movie/{id}?autoPlay=true", // Added based on typical pathing
+                movie: "{baseUrl}/movie/{id}?autoPlay=true",
                 tv: "{baseUrl}/tv/{id}/{season}/{episode}?autoPlay=true",
             },
         },
@@ -20,7 +32,7 @@ const extension: ExtensionManifest = {
             name: "2embed.online",
             enabled: true,
             required: false,
-            priority: 2,
+            priority: 3,
             baseUrl: "https://www.2embed.online/embed",
             embedPatterns: {
                 movie: "{baseUrl}/{id}",
@@ -32,7 +44,7 @@ const extension: ExtensionManifest = {
             name: "VidSrc.to",
             enabled: true,
             required: false,
-            priority: 3,
+            priority: 4,
             baseUrl: "https://vidsrc.to/embed",
             embedPatterns: {
                 movie: "{baseUrl}/movie/{id}",
@@ -44,7 +56,7 @@ const extension: ExtensionManifest = {
             name: "VidSrc.me",
             enabled: true,
             required: false,
-            priority: 4,
+            priority: 5,
             baseUrl: "https://vidsrc.me/embed",
             embedPatterns: {
                 movie: "{baseUrl}/movie?tmdb={id}",
@@ -56,14 +68,13 @@ const extension: ExtensionManifest = {
             name: "SuperEmbed",
             enabled: true,
             required: false,
-            priority: 5,
+            priority: 6,
             baseUrl: "https://multiembed.mov/directstream.php",
             embedPatterns: {
                 movie: "{baseUrl}?video_id={id}&tmdb=1",
                 tv: "{baseUrl}?video_id={id}&tmdb=1&s={season}&e={episode}",
             },
         },
-        
     ],
 }
 
